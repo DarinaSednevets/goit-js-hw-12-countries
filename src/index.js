2a288fddeae0f033bce99d3b0de1931fdd51fb86
DarinaSednevets <dariaausten@gmail.com>
2021-08-10 14:33:00 +0300

    add task

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import './sass/main.scss';
+// import './styles.css';
+// import fetchCountries from './js/fetchCountries.js'
+
+// fetchCountries();
+
+import './styles.css';
+import initUi from './js/uiService';
+
+initUi();
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
+
+
+export default function fetchCountries(searchQuery) {
+    const url = `${BASE_URL}${searchQuery}`;
+    return fetch(url).then(response => response.json());
+}
+
